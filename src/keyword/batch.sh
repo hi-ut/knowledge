883d6411ddea3a2167583b93612745bd38bc4e35
nakamura196 <na.kamura.1263@gmail.com>
2020-11-26 16:04:23 +0900

    update uri

---
+++ src/keyword/batch.sh
@@ -0,0 +1,3 @@
+python 110_getKotobank.py
+python 111_getWiki.py
+python 112_createRdf.py
---
+++ src/person/12_convertTei2GSheets.py
@@ -0,0 +1,185 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import xml.etree.ElementTree as ET
+import sys
+import urllib
+import json
+import argparse
+import urllib.request
+from rdflib import URIRef, BNode, Literal, Graph
+import glob
+import requests
+import os
+
+"""
+Shows basic usage of the Sheets API.
+Prints values from a sample spreadsheet.
+"""
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets','https://www.googleapis.com/auth/drive']
+
+creds = None
+# The file token.pickle stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists('token.pickle'):
+    with open('token.pickle', 'rb') as token:
+        creds = pickle.load(token)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+    else:
+        flow = InstalledAppFlow.from_client_secrets_file(
+            'credentials.json', SCOPES)
+        creds = flow.run_local_server(port=0)
+    # Save the credentials for the next run
+    with open('token.pickle', 'wb') as token:
+        pickle.dump(creds, token)
+
+service = build('sheets', 'v4', credentials=creds)
+
+spreadsheet_id = '1FX92zyp916uZBwoosE4oaXNBR0MeGay0l30SvHl5PFQ'
+
+# Call the Sheets API
+def read_data(spreadId, rangeName, service):
+    SPREADSHEET_ID = spreadId
+    RANGE_NAME = rangeName
+    result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+                                                 range=RANGE_NAME).execute()
+    return result
+
+sheets = ["藤原経光"] # , "abc"
+
+all = Graph()
+
+def bbb(value):
+
+    flg = False
+
+    if value.startswith("http"):
+        flg = True
+
+    if value.startswith("_:"):
+        flg = True
+
+    map = {
+        "jps" : "https://jpsearch.go.jp/term/property#",
+        "schema" : "http://schema.org/", 
+        "prop-ja" : "http://ja.dbpedia.org/property/",
+        "word" : "https://kotobank.jp/word/",
+        "emperor" : "https://nakamura196.github.io/hi_person/entity/emperor/",
+        "wiki": "https://ja.wikipedia.org/wiki/",
+        "kani" : "https://nakamura196.github.io/hi_person/term/kani/",
+        "person" : "https://nakamura196.github.io/hi_person/entity/chname/",
+        "role" : "https://nakamura196.github.io/hi_person/term/role/"
+    }
+
+    for key in map:
+        if key+":" in value:
+            flg = True
+            value = value.replace(key+":", map[key])
+
+            if key in ["emperor", "kani", "person", "role"]:
+                value += ".json"
+
+
+
+    if flg:
+        value = URIRef(value)
+    else:
+        value = Literal(value)
+
+    return value
+
+for sheetname in sheets:
+
+    g = Graph()
+
+    flg = False
+
+    result = read_data(spreadsheet_id, sheetname + "!A1:D1000", service)
+
+    values = result["values"]
+
+    subject_str = "https://nakamura196.github.io/hi_person/entity/chname/"+sheetname+".json"
+
+    subject = URIRef(subject_str)
+
+    map = {}
+
+    for i in range(1, len(values)):
+        row = values[i]
+
+
+        v = row[0]
+
+        if v == "":
+            continue
+
+        if v == "prop-ja:官位":
+            flg = True
+
+        v = bbb(v)
+        o = row[1]
+        
+
+        v2 = None
+        o2 = None
+
+        if not flg:
+            o = bbb(o)
+            all.add((subject, v, o))
+            g.add((subject, v, o))
+
+        else:
+            o = values[i][1]
+
+            map[o] = []
+
+            for j in range(0, 6):
+                row1 = values[i+j]
+
+                if len(row1) < 4:
+                    continue
+
+                p = row1[2]
+
+                values1 = row1[3].split("|")
+
+                for value1 in values1:
+                    map[o].append({
+                        "v" : bbb(p),
+                        "o" : bbb(value1)
+                    })
+
+    for key in map:
+        b = BNode()
+        v = URIRef(bbb("prop-ja:官位"))
+        # v = URIRef(bbb("jps:agential"))
+        # b = URIRef(bbb(subject_str+"#"+str(key).zfill(2)))
+        b = URIRef(bbb("_:"+sheetname+"_官位_"+str(key).zfill(3)))
+        all.add((subject, v, b))
+        g.add((subject, v, b))
+
+        for obj in map[key]:
+            all.add((b, obj["v"], obj["o"]))
+            g.add((b, obj["v"], obj["o"]))
+
+    path = subject_str.replace("https://nakamura196.github.io/hi_person", "../../docs2")
+    print(path)
+
+    dirname = os.path.dirname(path)
+
+    os.makedirs(dirname, exist_ok=True)
+
+    g.serialize(destination=path, format='json-ld')
+
+# print(all)
+all.serialize(destination="data/person.rdf", format='pretty-xml')
+# print(all.serialize(format="turtle").decode("utf-8"))
